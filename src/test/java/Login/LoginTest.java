22468a1e8f4e32b481c1aa1f44343d723ca5ece1
CODOID <er.n.dinesh2017@gmail.com>
2021-08-03 22:16:16 +0530

    Updated file

---
+++ src/test/java/Login/LoginTest.java
@@ -0,0 +1,33 @@
+package Login;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LoginTest {
+    public static void main(String[] args) throws IOException {
+
+        System.setProperty("webdriver.Chrome.driver", "F:\\Practise Intellij\\driver\\drivers\\ChromeDriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://facebook.com");
+
+        boolean fbLogo = driver.findElement(By.xpath("//img[@class]")).isDisplayed();
+
+        if(fbLogo = true){
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File("F:/Practise Intellij/images/screenshot/verify.png"));
+        }else{
+            System.out.println("User not visit to Facebook page");
+        }
+
+    }
+}
